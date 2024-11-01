01ff71189a2e057885af69cd34e875c7db9c3816
dieptv <diep.tv1999@gmail.com>
2024-11-01 22:09:39 +0700

    add feature get started

--- apps/client/src/components/sidebar.tsx
+++ apps/client/src/components/sidebar.tsx
@@ -7,6 +7,7 @@ import {cn} from '@client/lib/utils'
 import {sidelinks} from '@client/data/sidelinks'
 import SidebarTour from '@client/components/sidebar-tour'
 import SelectProject from "@client/pages/projects/components/select-project";
+import logo from '@client/assets/logoab.png'
 
 interface SidebarProps extends React.HTMLAttributes<HTMLElement> {
   isCollapsed: boolean
@@ -37,36 +38,7 @@ export default function Sidebar2({
         {/* Header */}
         <LayoutHeader className="sticky top-0 justify-between px-4 py-3 shadow md:px-4">
           <div className={`flex items-center ${!isCollapsed ? 'gap-2' : ''}`}>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 256 256"
-              className={`transition-all ${isCollapsed ? 'h-6 w-6' : 'h-8 w-8'}`}
-            >
-              <rect width="256" height="256" fill="none"></rect>
-              <line
-                x1="208"
-                y1="128"
-                x2="128"
-                y2="208"
-                fill="none"
-                stroke="currentColor"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="16"
-              ></line>
-              <line
-                x1="192"
-                y1="40"
-                x2="40"
-                y2="192"
-                fill="none"
-                stroke="currentColor"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="16"
-              ></line>
-              <span className="sr-only">Website Name</span>
-            </svg>
+            <img src={logo} alt={'logo'} width={36} height={36}/>
             <div
               className={`flex flex-col justify-end truncate ${isCollapsed ? 'invisible w-0' : 'visible w-auto'}`}
             >
